1715c1989090b7e22502df1153c07736d61c2eca
medicpip <119620872+medicpip@users.noreply.github.com>
2022-12-10 20:38:51 -0700

    Create config-sample.php

---
+++ php/config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+// CONFIGURATION FILE
+// This file holds the values to run the web application
+// This file should NOT be made public
+// This sample file is included for setting up the application
+// Rename this file to config.php when ready
+
+?>
